c3984775a5f30656b366c1d8c037461b943b5a7c
777 <777@HOME-PC>
2024-09-11 22:11:27 +0300

    Добавьте файлы проекта.

---
+++ Lab/fMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTabletsInfo = new System.Windows.Forms.TextBox();
+            this.btnAddTablet = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbTabletsInfo
+            // 
+            this.tbTabletsInfo.Location = new System.Drawing.Point(46, 54);
+            this.tbTabletsInfo.Multiline = true;
+            this.tbTabletsInfo.Name = "tbTabletsInfo";
+            this.tbTabletsInfo.ReadOnly = true;
+            this.tbTabletsInfo.Size = new System.Drawing.Size(438, 320);
+            this.tbTabletsInfo.TabIndex = 0;
+            // 
+            // btnAddTablet
+            // 
+            this.btnAddTablet.Location = new System.Drawing.Point(530, 54);
+            this.btnAddTablet.Name = "btnAddTablet";
+            this.btnAddTablet.Size = new System.Drawing.Size(155, 38);
+            this.btnAddTablet.TabIndex = 1;
+            this.btnAddTablet.Text = "Додати планшет";
+            this.btnAddTablet.UseVisualStyleBackColor = true;
+            this.btnAddTablet.Click += new System.EventHandler(this.btnAddTablet_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(530, 115);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(155, 39);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрити";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddTablet);
+            this.Controls.Add(this.tbTabletsInfo);
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTabletsInfo;
+        private System.Windows.Forms.Button btnAddTablet;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Lab/fMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddTablet_Click(object sender, EventArgs e)
+        {
+            Tablet tablet = new Tablet();
+            fTablet ft = new fTablet(tablet);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                tbTabletsInfo.Text +=
+                string.Format("Бренд: {0}. Ціна: {1} грн. Вага: {2} г. Колір: {3}. Діагональ екрана:" +
+                " {4:0.0} ''. Частота процесора: {5:0.0} ГГц. {6}  {7} " +
+                "Ціна планшета зі знижкою 30%: {8:0.00} грн. Ціна планшета зі знижкою 30% та знижкою постійного клієнта: {9:0.00} грн.\r\n",
+                tablet.brand, tablet.price, tablet.weight, tablet.color, tablet.screendiagonal,
+                tablet.CPUfrequency, tablet.isthereasimcard ? "Є sim-карта." : "Немає sim-карти.", tablet.isthereamemorycardslot ? "Є слот для карти пам'яті." : "Немає слота для карти пам'яті.", 
+                tablet.discountedPrice(), tablet.discountwitharegularcustomercard());
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Припинити роботу застосунку?", "Припинити роботу",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                Application.Exit();
+        }
+    }
+}
